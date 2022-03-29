81b74ba6e303e5c6e553bc8d5924ff76599abd6e
kuryankp <kuryankp@github.com>
2022-03-30 00:17:09 +1000

    builds, to machine

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,11 @@
 scp off.sh kuryan@192.168.120.128:/home/kuryan/off.sh
 ssh kuryan@192.168.120.128 chmod +x /home/kuryan/off.sh
 ssh kuryan@192.168.120.128 /home/kuryan/off.sh
+scp revivedb.sh kuryan@192.168.120.128:/home/kuryan/revivedb.sh
+ssh kuryan@192.168.120.128 chmod +x /home/kuryan/revivedb.sh
+scp dump_crone.sh kuryan@192.168.120.128:/home/kuryan/dump_crone.sh
+ssh kuryan@192.168.120.128 chmod +x /home/kuryan/dump_crone.sh
+
 ssh kuryan@192.168.120.128 docker-compose down
 ssh kuryan@192.168.120.128 docker pull kuryankp/adminprojectfrontend:v0.0.2
 ssh kuryan@192.168.120.128 docker pull kuryankp/adminprojectbackend:v0.0.1
