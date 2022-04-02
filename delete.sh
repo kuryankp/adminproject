5b844f25398f6943723edc9ca6c087f7b3cbef6f
kuryankp <kuryankp@github.com>
2022-04-02 18:50:55 +1000

    builds, to machine

--- delete.sh
+++ delete.sh
@@ -5,5 +5,6 @@
 docker image rm $(docker image ls -q)
 docker volume rm $(docker volume ls -q)
 
-rm cron docker-compose.yml dump-crone.sh monitor.sh off.sh revivedb.sh
+rm cron docker-compose.yml dump_crone.sh monitor.sh off.sh revivedb.sh
 echo 228 | sudo -S snap remove docker
+rm delete.sh
